09ea403d77855148ca49fc939b15edbcfe222033
Zytronium <dastelljes@outlook.com>
2025-07-16 17:10:52 -0500

    Simplified routes in routes/index.js. This silences a weak warning from my IDE, follows best practices, and looks cleaner.

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,8 @@ import AppController from '../controllers/AppController';
 
 const router = express.Router();
 
-router.get('/status', (req, res) => {
-  AppController.getStatus(req, res);
-});
+router.get('/status', AppController.getStatus);
 
-router.get('/stats', (req, res) => {
-  AppController.getStats(req, res);
-});
+router.get('/stats', AppController.getStats);
 
 export default router;
